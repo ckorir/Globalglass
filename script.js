ccebf56f62ede67ae2b926ebef438507ba0fd029
ckorir <chrisruto.k@gmail.com>
2021-05-29 11:27:11 +0300

    added another projects page

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const opennav = document.querySelector('.burger');
 const closenav = document.querySelector('.close');
 const items = document.querySelector('.links');
 
-opennav.addEventListener('click',show);
-closenav.addEventListener('click',close);
-
 function show(){ 
     items.style.display = 'block';
     opennav.style.display = 'none';
